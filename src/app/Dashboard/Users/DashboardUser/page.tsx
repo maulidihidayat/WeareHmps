c08d9a5d8b4920ad8058c69d60da20d458bfd041
maulidihidayat <ahmadhapinuddin@gmail.com>
2024-11-25 14:59:50 +0800

    user

---
+++ src/app/Dashboard/Users/DashboardUser/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function DashboardUser() {
+  return (
+    <div className="flex justify-center h-auto mb-10 ml-32">
+        <div className="w-[902px] items-center p-10 rounded-2xl border-2 max-w-3xl">
+            <h1 className="text-white font-bold border-2 border-b-[7px] border-secondary rounded-xl bg-primary  mb-6 py-4  text-center">SELAMAT DATANG DI HMPS PENDIDIKAN INFORMATIKA</h1>
+        </div>
+    </div>
+  )
+}
